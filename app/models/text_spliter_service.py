2bad391225961455e5a0efe1fe0334027995ba95
Luo <2066803145@qq.com>
2026-06-23 12:59:47 +0800

    "update path"

---
+++ app/models/text_spliter_service.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+class BaseInfo(BaseModel):
+    name:str |None=None
+    phone:str |None=None
+    email:str |None=None
+    gender:str |None=None
+    age:str |None=None
+    city:str |None=None
+
+class Skills(BaseModel):
+    programming:list[str]|None=None
+    frameworks:list[str]|None=None
+    databases:list[str]|None=None
+    middleware:list[str]|None=None
+    tools:list[str]|None=None
+
+class EducationData(BaseModel):
+    school:str|None=None
+    major:str|None=None
+    degree:str|None=None
+    startTime:str|None=None
+    endTime:str|None=None
+    gpa:str|None=None
+    rank:str|None=None
+
+class Education(BaseModel):
+    education:list[EducationData]
+
+class ResumeText(BaseModel):
+    baseInfo:Optional[BaseInfo]=None
+    skills:Optional[Skills]=None
+    education:Optional[Education]=None
+    projects:str|None=None
+    awards:str|None=None
+    overallSummary:str|None=None
+    workExperience:str|None=None
+    resumId:str
