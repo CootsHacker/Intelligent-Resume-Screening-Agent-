8bdca5d72105849de126e7e86bccc3322a97b3e2
Luo <2066803145@qq.com>
2026-07-01 21:36:57 +0800

    "update vector_database.py"

--- app/models/vector_database.py
+++ app/models/vector_database.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 class VectorData(BaseModel):
+    resumeId: str
     overallSummary: str | None = None
     workExperience: str | None = None
     projectExperience: str | None = None
-    resumeId:str
